0a932d8a7145d6e71317b590b921ff3b8d0c65f8
patrickatcw <pj8392@yahoo.com>
2018-03-01 12:05:09 -0500

    added ford and vw sub-classes with print logic, and results

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -72,6 +72,89 @@ class Chevy extends Car {
         super(cylinders, name);
     }
 
+    //step 11 override now, just chose the following 3
+    @Override
+    public String startEngine() {
+        //return super.startEngine();       change to....
+        return "Chevy -> startEngine()";
+
+    }
+
+    @Override
+    public String accelerate() {
+        //return super.accelerate();        changed to....
+        return "Chevy -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        //return super.brake();             changed to.....
+        return "Chevy -> brake()";
+
+        //step 12 test for car and chevy in main below
+    }
+
+//all of the above is within chevy sub-class
+}
+
+//step 14 creating another class, ford, copy and pasted chevy, the changed some inside
+class Ford extends Car {
+
+    public Ford(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        //return "Ford -> startEngine()";
+        //another way of getting above
+        return getClass().getSimpleName() + " -> startEngine()";
+        //get.Class is an automatic extension, hit control button a mouse over
+        //get.Close
+
+    }
+
+    @Override
+    public String accelerate() {
+        return "Ford -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Ford -> brake()";
+
+        //step 15 test these
+    }
+}
+
+//step 16 creating another class, ford, copy and pasted chevy, the changed some inside
+class VW extends Car {
+
+    public VW(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        //return "VW -> startEngine()";
+        //another way of getting above
+        return getClass().getSimpleName() + " -> startEngine()";
+        //get.Class is an automatic extension, hit control button a mouse over
+        //get.Close
+
+    }
+
+    @Override
+    public String accelerate() {
+        return "VW -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "VW -> brake()";
+
+        //step 17 test these by running in main
+    }
 }
 
 
@@ -79,6 +162,58 @@ class Chevy extends Car {
 public class Main {
 
     public static void main(String[] args) {
+
+        //step 12 testing for car
+        Car car = new Car(8, "Base car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+        //result
+                /*
+                Car -> startEngine()
+                Car -> accelerate()
+                Car -> brake()
+                */
+
+        //step 13 testing for chevy
+        Chevy chevy = new Chevy(6, "Chevette");
+        System.out.println(chevy.startEngine());
+        System.out.println(chevy.accelerate());
+        System.out.println(chevy.brake());
+        //results
+                /*
+                Chevy -> startEngine()
+                Chevy -> accelerate()
+                Chevy -> brake()
+                */
+
+         //now add another car-type Ford as a subclass to base car class, step 14
+
+        //step 15 testing for ford
+        Ford ford = new Ford(6, "Falcon");
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+        //results;
+                /*
+                Ford -> startEngine()
+                Ford -> accelerate()
+                Ford -> brake()
+                */
+
+          //now add another car-type VW as a subclass to base car class, step16
+
+        //step 17 testing for ford
+        VW vw = new VW (4, "Jetta");
+        System.out.println(vw.startEngine());
+        System.out.println(vw.accelerate());
+        System.out.println(vw.brake());
+        //results;
+                /*
+                VW -> startEngine()
+                VW -> accelerate()
+                VW -> brake()
+                */
 
     }
 
