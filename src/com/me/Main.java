2b6bcd78f40b0134416cc193f020c941fd61fdb5
patrickatcw <pj8392@yahoo.com>
2018-03-01 11:32:54 -0500

    created a base class called car

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -14,6 +14,29 @@ message for each in the base class
 - put all classes in the one java file, like this one we are in now!
  */
 
+//step 1 create Car class
+class Car {
+
+    //step 2 create fields
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    //step 3 create constructor, only doing cylinders and name,
+    // then set values manually for wheels and engine
+    public Car(int cylinders, String name) {
+        this.cylinders = cylinders;
+        this.name = name;
+        //step 4 set values for the following we did not put in constructor
+        this.wheels = 4;
+        this.engine = true;
+
+    }
+
+}
+
+
 public class Main {
 
     public static void main(String[] args) {
