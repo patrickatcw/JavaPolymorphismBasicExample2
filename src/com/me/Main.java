42b81335045abd79337181d16c620abcb77c68d2
patrickatcw <pj8392@yahoo.com>
2018-03-01 11:30:42 -0500

    initial structure, update gitignore, directions

---
+++ src/com/me/Main.java
@@ -0,0 +1,23 @@
+package com.me;
+//directions
+/*
+- create a base class called car
+- it should have a few fields that would be appropriate for a generic
+car class; engine, cylinders, wheels, etc.
+- constructor should initialize # of cylinders and name, and set wheels to 4
+and engine to true. cylinders and names would be passed parameters
+- create appropriate getters
+- create some methods like startEngine, accelerate, and brake; show a
+message for each in the base class
+- now create 3 sub classes for your favorite vehicles
+- override the appropriate methods to demonstrate polymorphism in use
+- put all classes in the one java file, like this one we are in now!
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
